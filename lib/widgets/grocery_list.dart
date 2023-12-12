acdbdad518ed36fd1edf722ece2bcd16ecf202d3
kurylko <reviliya@gmail.com>
2023-12-12 18:26:13 +0100

    user can add and remove list items

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -25,14 +25,47 @@ class _GroceryListState extends State<GroceryList> {
       return;
     }
 
-    //getting new item for list 
+    //getting new item for list
     setState(() {
       _groceryItems.add(newItem);
     });
   }
 
+  void _removeItem(GroceryItem) {
+    setState(() {
+      _groceryItems.remove(item);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget content = const Center(
+      child: Text('No items yet'),
+    );
+
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (ctx, index) => Dismissible( //to swipe away item 
+          onDismissed: (direction) {
+            _removeItem(_groceryItems[index]);
+          },
+          key: ValueKey(_groceryItems[index].id),
+          child: ListTile(
+            title: Text(_groceryItems[index].name),
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: _groceryItems[index].category.color,
+            ),
+            trailing: Text(
+              _groceryItems[index].quantity.toString(),
+            ),
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('My groceries'),
@@ -43,20 +76,7 @@ class _GroceryListState extends State<GroceryList> {
           ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: _groceryItems.length,
-        itemBuilder: (ctx, index) => ListTile(
-          title: Text(_groceryItems[index].name),
-          leading: Container(
-            width: 24,
-            height: 24,
-            color: _groceryItems[index].category.color,
-          ),
-          trailing: Text(
-            _groceryItems[index].quantity.toString(),
-          ),
-        ),
-      ),
+      body: content,
     );
   }
 }
